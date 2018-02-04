2b6bf22fffa51f7344c1a45f4e12918caedb5bab
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-02-04 21:39:16 +0900

    コメント追加

--- main.js
+++ main.js
@@ -4,27 +4,47 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const uuid = require('uuid/v4')
 
+// =====================================
+// 初期設定
+// =====================================
+
 const app = express()
 
+// POSTデータを扱うためのミドルウェアを登録
 app.use(bodyParser.urlencoded({ extended: true }))
+// クッキーを扱うためのミドルウェアを登録
 app.use(cookieParser())
+// 静的ファイルの配置場所はpublicディレクトリ
 app.use(express.static('public'))
+// テンプレートエンジンにはEJSを使用する
 app.set('view engine', 'ejs')
 
+// クッキーに使うキー名
 const SESSID_COOKIE_KEYNAME = 'LEARNING_HTTP_SESSID'
 
+// =====================================
+// ルーティング
+// =====================================
+
+/**
+ * GETリクエスト受信時
+ */
 app.get('/', (req, res) => {
   let name = null
 
   // Cookieが存在するか
   const sessionId = req.cookies[SESSID_COOKIE_KEYNAME]
   if (sessionId && fs.existsSync(`./session/${sessionId}`)) {
+    // セッションファイルの内容を読み取る
     name = fs.readFileSync(`./session/${sessionId}`, 'utf8')
   }
 
   res.render('index', { name })
 })
 
+/**
+ * POSTリクエスト受信時
+ */
 app.post('/', (req, res) => {
   // 送信されたフォームの内容を取り出す
   const name = req.body.name
@@ -40,6 +60,10 @@ app.post('/', (req, res) => {
   // トップ画面にリダイレクト
   res.redirect('/')
 })
+
+// =====================================
+// 起動
+// =====================================
 
 const port = process.env.PORT || 3000
 
